a4c8b673420f00384886c446f27cb9bfe09bd5be
Ricardo Alexandro Mena <ricardoamena@gmail.com>
2024-04-14 23:45:15 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import inicialeslogo from './imagenes/iniciales3.jpg'
+import Boton from './componentes/boton';
+
 
 function App() {
+
+  const manejarClick = () => {
+    console.log('Click');
+  }
+
+  const reiniciarCont = () => {
+    console.log ('Reinicar')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={inicialeslogo} alt='Logo de freecodecamp'/>
+      </div> 
+      <div className='contenedor-principal'>
+        <Boton
+        texto='Click'
+        esBotonDeClick={true}
+        manejarClick={manejarClick}
+        />
+        <Boton
+        texto='Reiniciar'
+        esBotonDeClick={false}
+        manejarClick={reiniciarCont}
+        />
+      
+      </div>
+      
     </div>
   );
 }
